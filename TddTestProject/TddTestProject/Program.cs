b77ac70337ae8f5ce9a19ac1cdad9ca20cb0d524
ayzhusain <ayzhusain@gmail.com>
2016-02-18 13:58:12 +0100

    LastCommit

--- TddTestProject/TddTestProject/Program.cs
+++ TddTestProject/TddTestProject/Program.cs
@@ -8,8 +8,100 @@ namespace TddTestProject
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Immigrants objImmigrants = new Immigrants();
+            objImmigrants.setStartYear();
+            objImmigrants.setEndYear();
+
+           // objImmigrants.addImmigrants();
+            objImmigrants.printNumberOfImmigrants();
+            Console.ReadKey();
+
+
         }
+    }
+    public class Immigrants
+    {
+        private double startyear;
+        private double endyear;
+        private double immigrants =26000.0;
+        private double incoming =300.0;
+        private double outgoing =325.0;
+        public Immigrants()
+        { }
+
+        public double setStartYear()
+        {
+            startyear = Console.Read();
+            return startyear;
+        }
+        public double setEndYear()
+        {
+            endyear = Console.Read();
+            return endyear;
+        }
+        public double getStartYear()
+        {
+           
+            if (startyear > endyear)
+            {
+                return 0;
+            }
+            return startyear;
+        }
+        public double getEndYear()
+        {
+            if (startyear < endyear)
+            {
+                return 0;
+            }
+            return endyear;
+        }
+        public void addImmigrants(double x)
+        {
+            immigrants = x;
+        }
+
+        public double getAddImmigrants()
+        {
+            return immigrants;
+        }
+        public void addIncoming(double y)
+        {
+            incoming = y;
+            
+        }
+
+        public double getIncomingImmigrants()
+        {
+            return incoming;
+        }
+
+        public void addOutgoing(double z)
+        { 
+            outgoing=z;
+        }
+        public double getOutgoingImmigrants()
+        {
+            return outgoing;
+        }
+
+
+        public void printNumberOfImmigrants()
+        {
+            for(double i =getStartYear(); getStartYear()<getEndYear();i++)
+            {
+                double born = 0.007 * immigrants;
+                double died = 0.006 * immigrants;
+                immigrants = immigrants + (incoming - outgoing) + (born - died);
+                
+            }
+            Console.WriteLine("Total Immigrants: " + immigrants);
+        }
+
+
+
     }
 }
--- TddTestProject/UnitTest/UnitTest1.cs
+++ TddTestProject/UnitTest/UnitTest1.cs
@@ -1,14 +1,95 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddTestProject;
+using TDDQuestion2;
 
 namespace UnitTest
 {
+ 
+
     [TestClass]
-    public class UnitTest1
+    public class UnitTestQuestion1
     {
-        [TestMethod]
-        public void TestMethod1()
+        Immigrants immi;
+
+        [TestInitialize]
+        public void Setup()
         {
+            immi = new Immigrants();
+        }
+
+        [TestMethod]
+        public void TestSatrtYear()
+        {
+            Assert.IsFalse(Convert.ToBoolean(immi.getStartYear()));
+            Assert.AreEqual(immi.getStartYear(),immi.getStartYear());
+            Assert.IsNotNull(immi.getStartYear());
+        }
+
+        [TestMethod]
+        public void TestEndtYear()
+        {
+            Assert.IsFalse(Convert.ToBoolean(immi.getEndYear()));
+            Assert.AreEqual(immi.setEndYear(),immi.getEndYear());
+            Assert.IsNotNull(immi.getEndYear());
+        }
+
+        [TestMethod]
+        public void TestNumberOfImmigrants()
+        {
+            double x = immi.getAddImmigrants();
+            Assert.AreEqual(x, immi.getAddImmigrants());
+        }
+        [TestMethod]
+        public void TestNumberOfImmigrantsIncoming()
+        {
+            double x = immi.getIncomingImmigrants();
+            Assert.AreEqual(x,immi.getIncomingImmigrants());
+        }
+
+        [TestMethod]
+        public void TestNumberOfImmigrantsOutgoing()
+        {
+            Assert.AreEqual(300, immi.getIncomingImmigrants());
         }
     }
+
+    [TestClass]
+    public class UnitTestQuestion2
+    {
+        Temperature objtemper;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            objtemper = new Temperature();
+        }
+
+        [TestMethod]
+        public void TestsetTemprature()
+        {
+            objtemper.setTemp(20);
+            double x = objtemper.getTemp();
+            Assert.AreEqual(20, x);
+        }
+
+        [TestMethod]
+        public void TestsetTempratureCold()
+        {
+            objtemper.setTemp(-50);
+            double x = objtemper.getTemp();
+            Assert.AreEqual(-50, x);
+        }
+
+        [TestMethod]
+        public void TestsetTempratureWarm()
+        {
+            objtemper.setTemp(50);
+            double x = objtemper.getTemp();
+            Assert.AreEqual(50, x);
+        }
+    }
+
+
+
 }
